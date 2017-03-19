a6346b435e7b507cd3e12bfde6ea43837d47f19b
Salumão Barbosa da Costa <salubcosta@gmail.com>
2017-03-19 00:00:21 -0300

    template do sistema

---
+++ views/template.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="uft8">
+	<title>Geração de Cartão</title>
+</head>
+<body>
+
+<?php $this->carregarView($view, $array); ?>
+</body>
+</html>
