c3d66891b5d01f1c39e9bccb6d8fb32563a085eb
Salumao Barbosa da Costa <salubcosta@gmail.com>
2017-03-20 18:35:29 -0300

    criação da view cartaofrente

---
+++ views/cartaofrente.php
@@ -0,0 +1,11 @@
+<div class="painel text-center">
+	<h4 class="text-info">Abrir Cartão na mesma ABA</h4>
+	<a href="<?php echo URL;?>/assets/imagens/cartaofrente.PNG" type="submit" class="btn btn-primary" target="_self">
+		<span class="glyphicon glyphicon-floppy-save"></span> Gerar Cartão - Frente
+	</a>
+<hr />
+	<h4 class="text-danger">Abrir Cartão em Nova ABA</h4>
+	<a href="<?php echo URL;?>/assets/imagens/cartaofrente.PNG" type="submit" class="btn btn-primary" target="_blank">
+		<span class="glyphicon glyphicon-floppy-save"></span> Gerar Cartão - Frente
+	</a>
+</div>
