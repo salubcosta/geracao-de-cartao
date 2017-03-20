8e557a6348f17a776bb69b96f833505839ac144a
Salumão Barbosa da Costa <salubcosta@gmail.com>
2017-03-20 00:07:58 -0300

    atualizaçao do menu, inclusão do dropdown menu.

--- views/template.php
+++ views/template.php
@@ -23,8 +23,14 @@
         </div>
         <div id="navbar" class="navbar-collapse collapse">
           <ul class="nav navbar-nav">
-      		<li><a href="<?php echo URL; ?>/home">Início</a></li>
-            <li><a href="<?php echo URL; ?>/cartao">Gerar Cartão</a></li>
+        		<li><a href="<?php echo URL; ?>/home">Início</a></li>
+            <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Gerar Cartão <span class="caret"></span></a>
+              <ul class="dropdown-menu">
+                <li><a href="<?php echo URL; ?>/cartaofrente">Cartão Frente</a></li>
+                <li><a href="<?php echo URL; ?>/cartao">Cartão Verso</a></li>
+              </ul>
+            </li>
           </ul>
         </div>
     </div>
