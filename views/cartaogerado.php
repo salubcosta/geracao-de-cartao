1d82e70ffe48d8af319470494902402cb1800594
Salumão Barbosa da Costa <salubcosta@gmail.com>
2017-03-21 23:38:40 -0300

    atualização

--- views/cartaogerado.php
+++ views/cartaogerado.php
@@ -2,12 +2,12 @@
 	<h3 class="text-success">Cartão-Verso disponível para baixar <span class="glyphicon glyphicon-picture"></span></h3>
 	<hr />
 	<h4 class="text-info">Abrir Cartão-verso na mesma ABA</h4>
-	<a href="<?php echo URL;?>/assets/imagens/cartaogerado.PNG" type="submit" class="btn btn-primary" target="_self">
+	<a href="<?php echo URL;?>/assets/imagens/cartaogerado.png" type="submit" class="btn btn-primary" target="_self">
 		<span class="glyphicon glyphicon-floppy-save"></span> Abrir Cartão - Verso
 	</a>
 <hr />
 	<h4 class="text-danger">Abrir Cartão-verso em Nova ABA</h4>
-	<a href="<?php echo URL;?>/assets/imagens/cartaogerado.PNG" type="submit" class="btn btn-primary" target="_blank">
+	<a href="<?php echo URL;?>/assets/imagens/cartaogerado.png" type="submit" class="btn btn-primary" target="_blank">
 		<span class="glyphicon glyphicon-floppy-save"></span> Abrir Cartão - Verso
 	</a>
 </div>
