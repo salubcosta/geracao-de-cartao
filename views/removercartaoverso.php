737701e99344aa3a650bc71777bc5fc0f3d0cc28
Salumao Barbosa da Costa <salubcosta@gmail.com>
2017-03-27 17:53:04 -0300

    atualizaçõa no layout

--- views/removercartaoverso.php
+++ views/removercartaoverso.php
@@ -3,7 +3,7 @@
 	$urlRemoverCartaoVerso =  !$cartao->verificarCartaoVerso() ? "'#'"." disabled=\"true\"" : "'".URL."/deleteimg'";
 ?>
 <div class="painel">
-	<h3 class="text-danger">Você tem certeza que deseja excluir o cartão-verso?</h3>
+	<h3 class="text-danger">Você tem certeza que deseja excluir o cartão?</h3>
 	<hr />
-	Remover Cartão-Verso gerado: <a href=<?php echo $urlRemoverCartaoVerso;?> class="btn btn-danger"><span class="glyphicon glyphicon-trash"></span> Remover Cartão-Verso</a>
+	Remover Cartão gerado: <a href=<?php echo $urlRemoverCartaoVerso;?> class="btn btn-danger"><span class="glyphicon glyphicon-trash"></span> Remover Cartão</a>
 </div>
