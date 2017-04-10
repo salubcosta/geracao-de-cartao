29580ba0e0571c527c8fc8deec4fa732b6c39fd1
Salumao Barbosa da Costa <salubcosta@gmail.com>
2017-04-10 16:24:40 -0300

    atualização da view home.php

--- views/home.php
+++ views/home.php
@@ -1,33 +1,34 @@
-<div class="painel">
-	<h4 class="text-info">Olá, seja bem-vindo!</h4>
+<div class="painel text-center">
+	<h3 class="text-info text-center">Olá, seja bem-vindo!</h3>
 	<hr />
 	<?php
 		$cartao = new cartao();
 		$urlCartaoVerso =  
 			!$cartao->verificarCartaoVerso() ? 
-				"'".URL."/home'"." class=\"btn btn-primary disabled\"" 
+				"'".URL."/home'"." class=\"btn btn-primary btn-lg disabled\"" 
 			: 
-				"'".URL."/cartaogerado' class=\"btn btn-primary\"";
+				"'".URL."/cartaogerado' class=\"btn btn-primary btn-lg\"";
 		
 		$urlRemoverCartaoVerso =  
 			!$cartao->verificarCartaoVerso() ? 
-				"'".URL."/home'"." class=\"btn btn-danger disabled\"" 
+				"'".URL."/home'"." class=\"btn btn-danger btn-lg disabled\"" 
 			: 
-				"'".URL."/removercartaoverso' class=\"btn btn-danger\"";
+				"'".URL."/removercartaoverso' class=\"btn btn-danger btn-lg\"";
 	?>
 	<p class="text-muted">
-		Baixar Cartão-Frente disponível: <a href="<?php echo URL;?>/cartaofrente" class="btn btn-primary"><span class="glyphicon glyphicon-download-alt"></span> Cartão-Frente</a>
-	</p>
-	<p class="text-muted">
-		Baixar Último Cartão Gerado: &nbsp;<a href=<?php echo $urlCartaoVerso ;?>><span class="glyphicon glyphicon-download-alt"></span> Baixar Cartão</a>
+		Baixar Cartão-Frente disponível:<br /> <a href="<?php echo URL;?>/cartaofrente" class="btn btn-primary btn-lg"><span class="glyphicon glyphicon-download-alt"></span> Cartão-Frente</a>
 	</p>
 	<hr />
 	<p class="text-muted">
-		Geração de novo Cartão: <a href="<?php echo URL;?>/cartaoverso" class="btn btn-success"><span class="glyphicon glyphicon-plus"></span> Geração de Cartão</a>
+		Baixar Último Cartão Gerado:<br /><a href=<?php echo $urlCartaoVerso ;?>><span class="glyphicon glyphicon-download-alt"></span> Baixar Cartão</a>
 	</p>
 	<hr />
 	<p class="text-muted">
-		Remover Último Cartão gerado: <a href=<?php echo $urlRemoverCartaoVerso;?>><span class="glyphicon glyphicon-trash"></span> Remover Cartão</a>
+		Geração de novo Cartão:<br /> <a href="<?php echo URL;?>/cartaoverso" class="btn btn-lg btn-primary"><span class="glyphicon glyphicon-plus"></span> Geração de Cartão</a>
+	</p>
+	<hr />
+	<p class="text-muted">
+		Remover Último Cartão gerado:<br /> <a href=<?php echo $urlRemoverCartaoVerso;?>><span class="glyphicon glyphicon-trash"></span> Remover Cartão</a>
 	</p>
 	
 	
